c34c304e20cddf120c1d25f217447eab8a3c65a0
marquesfelipesilva <marquesfelipesilva@gmail.com>
2015-06-20 01:04:51 -0300

    inserindo combos nos campos NO_CIDADE e SIGLA_UF, tentando corrigir erro ao tentar cadastrar

--- view/inserirFornecedor.php
+++ view/inserirFornecedor.php
@@ -17,7 +17,7 @@ $cc = new ControlFornecedor();
 
 if (isset($_POST["inserir"])) {
     #passa os dados para inserir
-    $cc->inserir($_POST["COD_FORNECEDORES"],$_POST["NO_FORNECEDOR"], $_POST["NO_FANTASIA"], $_POST["RAZAO_SOCIAL"], $_POST["NU_CNPJ"], $_POST["NO_CIDADE"], $_POST["DS_ENDERECO"], $_POST["DS_COMPLMENTO_END"], $_POST["SIGLA_UF"],$_POST["NU_TELEFONE1"],$_POST["NU_TELEFONE2"],$_POST["NU_TELEFONE3"],$_POST["DS_EMAIL"],$_POST["DS_OBSERVACAO"],$_POST["DT_CADASTRO"] );
+    $cc->inserir($_POST["COD_FORNECEDORES"], $_POST["NO_FORNECEDOR"], $_POST["NO_FANTASIA"], $_POST["RAZAO_SOCIAL"], $_POST["NU_CNPJ"], $_POST["NO_CIDADE"], $_POST["DS_ENDERECO"], $_POST["DS_COMPLMENTO_END"], $_POST["SIGLA_UF"], $_POST["NU_TELEFONE1"], $_POST["NU_TELEFONE2"], $_POST["NU_TELEFONE3"], $_POST["DS_EMAIL"], $_POST["DS_OBSERVACAO"], $_POST["DT_CADASTRO"]);
     #redirecionar
     header("location: ../view/consultarFornecedor.php");
 }
@@ -71,85 +71,143 @@ if (isset($_POST["inserir"])) {
                                         </div>
                                     </div>
                                     <div class="control-group">
-                                        <label class="control-label" for="NO_CIDADE">Cidade:</label>
+                                        <label class="control-label" for="NO_CIDADE">Estado:</label>
                                         <div class="controls">
-                                            <input class="span6" name="NO_CIDADE" type="text" id="NO_CIDADE"  />
-                                        </div>
+                                            <select name="NO_CIDADE">
+                                            <option value="estado">Selecione o Estado</option> 
+                                            <option value="ac">Acre</option> 
+                                            <option value="al">Alagoas</option> 
+                                            <option value="am">Amazonas</option> 
+                                            <option value="ap">Amapá</option> 
+                                            <option value="ba">Bahia</option> 
+                                            <option value="ce">Ceará</option> 
+                                            <option value="df">Distrito Federal</option> 
+                                            <option value="es">Espírito Santo</option> 
+                                            <option value="go">Goiás</option> 
+                                            <option value="ma">Maranhão</option> 
+                                            <option value="mt">Mato Grosso</option> 
+                                            <option value="ms">Mato Grosso do Sul</option> 
+                                            <option value="mg">Minas Gerais</option> 
+                                            <option value="pa">Pará</option> 
+                                            <option value="pb">Paraíba</option> 
+                                            <option value="pr">Paraná</option> 
+                                            <option value="pe">Pernambuco</option> 
+                                            <option value="pi">Piauí</option> 
+                                            <option value="rj">Rio de Janeiro</option> 
+                                            <option value="rn">Rio Grande do Norte</option> 
+                                            <option value="ro">Rondônia</option> 
+                                            <option value="rs">Rio Grande do Sul</option> 
+                                            <option value="rr">Roraima</option> 
+                                            <option value="sc">Santa Catarina</option> 
+                                            <option value="se">Sergipe</option> 
+                                            <option value="sp">São Paulo</option> 
+                                            <option value="to">Tocantins</option>
+                                            </select>
+                                            </div>
                                     </div>
-                                    
+
                                     <div class="control-group">
                                         <label class="control-label" for="DS_ENDERECO">Endereço:</label>
                                         <div class="controls">
                                             <input class="span6" name="DS_ENDERECO" type="text" id="DS_ENDERECO"  />
                                         </div>
                                     </div>
-                                    
+
                                     <div class="control-group">
                                         <label class="control-label" for="DS_COMPLMENTO_END">Complemento Endereço:</label>
                                         <div class="controls">
                                             <input class="span6" name="DS_COMPLMENTO_END" type="text" id="DS_COMPLMENTO_END"  />
                                         </div>
                                     </div>
-                                    
+
                                     <div class="control-group">
-                                        <label class="control-label" for="SIGLA_UF">Sigla da Cidade:</label>
+                                        <label class="control-label" for="SIGLA_UF">Sigla UF:</label>
                                         <div class="controls">
-                                            <input class="span6" name="SIGLA_UF" type="text" id="SIGLA_UF" size="2" />
-                                        </div>
+                                            <select name="SIGLA_UF">
+                                                <option>Selecione...</option>
+                                                <option value="AC">AC</option>
+                                                <option value="AL">AL</option>
+                                                <option value="AM">AM</option>
+                                                <option value="AP">AP</option>
+                                                <option value="BA">BA</option>
+                                                <option value="CE">CE</option>
+                                                <option value="DF">DF</option>
+                                                <option value="ES">ES</option>
+                                                <option value="GO">GO</option>
+                                                <option value="MA">MA</option>
+                                                <option value="MG">MG</option>
+                                                <option value="MS">MS</option>
+                                                <option value="MT">MT</option>
+                                                <option value="PA">PA</option>
+                                                <option value="PB">PB</option>
+                                                <option value="PE">PE</option>
+                                                <option value="PI">PI</option>
+                                                <option value="PR">PR</option>
+                                                <option value="RJ">RJ</option>
+                                                <option value="RN">RN</option>
+                                                <option value="RO">RO</option>
+                                                <option value="RR">RR</option>
+                                                <option value="RS">RS</option>
+                                                <option value="SC">SC</option>
+                                                <option value="SE">SE</option>
+                                                <option value="SP">SP</option>
+                                                <option value="TO">TO</option>
+                                            </select>
+                                            </div>
                                     </div>
-                                    
+
                                     <div class="control-group">
                                         <label class="control-label" for="NU_TELEFONE1">Telefone Fixo:</label>
                                         <div class="controls">
                                             <input class="span6" name="NU_TELEFONE1" type="text" id="NU_TELEFONE1" />
                                         </div>
                                     </div>
-                                    
+
                                     <div class="control-group">
                                         <label class="control-label" for="NU_TELEFONE2">Telefone Móvel:</label>
                                         <div class="controls">
                                             <input class="span6" name="NU_TELEFONE2" type="text" id="NU_TELEFONE2" />
                                         </div>
                                     </div>
-                                    
+
                                     <div class="control-group">
                                         <label class="control-label" for="NU_TELEFONE3">Telefone Recado:</label>
                                         <div class="controls">
                                             <input class="span6" name="NU_TELEFONE3" type="text" id="NU_TELEFONE3" />
                                         </div>
                                     </div>
-                                    
+
                                     <div class="control-group">
                                         <label class="control-label" for="DS_EMAIL">E-mail:</label>
                                         <div class="controls">
                                             <input class="span6" name="DS_EMAIL" type="text" id="DS_EMAIL" />
                                         </div>
                                     </div>
-                                    
+
                                     <div class="control-group">
                                         <label class="control-label" for="DS_OBSERVACAO">Observação:</label>
                                         <div class="controls">
                                             <input class="span6" name="DS_OBSERVACAO" type="text" id="DS_OBSERVACAO"  />
                                         </div>
                                     </div>
-                                    
+
                                     <div class="control-group">
                                         <label class="control-label" for="DT_CADASTRO">Data do Cadastro:</label>
                                         <div class="controls">
                                             <input class="span6" name="DT_CADASTRO" type="date" id="DT_CADASTRO" />
                                         </div>
                                     </div>
-                                    
+
                                     <div class="control-group">
                                         <div class="controls">
                                             <button type="submit" name="inserir" class="btn btn-primary"> Cadastrar</i></button>
 
-                                   </div>
-                           </div>
-                     </form>
-            </fieldset>
-      </body>
-  
-</html>
+                                        </div>
+                                    </div>
+                                </form>
+                            </fieldset>
+                            </body>
 
-        
+                            </html>
+
+
