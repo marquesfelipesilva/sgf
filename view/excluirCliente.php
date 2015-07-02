67227ab34cbbce3998e95d38b2e91303140425a6
marquesfelipesilva <marquesfelipesilva@gmail.com>
2015-07-02 20:19:01 -0300

    Tentando fazer excluir cliente

--- view/excluirCliente.php
+++ view/excluirCliente.php
@@ -21,7 +21,6 @@ if (isset($_POST["excluir"])) {
     #passa o id e nome para consultar
     $cc->excluir($_POST["COD_CLIENTE"]);
 }
-
 ?>
 <html>
     <head>
@@ -41,65 +40,66 @@ if (isset($_POST["excluir"])) {
         #mostrar os dados do cliente
         foreach ($clientes as $item) {
             ?>
-        
-        
-        <div id="container-geral">
-            <div class="container" id="content">
-                <div class="container-fluid">
-                    <div class="row-fluid">
-                        <div>
-                            <fieldset>
-                                <legend>Excluir Cliente</legend>
-                                <form id="cliente" name="excluir" method="post" action="excluirCliente.php">
-                                    <div class="control-group">
-                                        <label class="control-label" for="COD_CLIENTE">Código:</label>
-                                        <div class="controls">
-                                            <input class="span6" name="COD_CLIENTE" readonly="true" id="COD_CLIENTE" value="<?php echo $item["COD_CLIENTE"]; ?>" />
-                                        </div>
-                                    </div>
-                                    <div class="control-group">
-                                        <label class="control-label" for="NO_CLIENTE">Nome:</label>
-                                        <div class="controls">
-                                            <input class="span6" name="NO_CLIENTE" type="text" id="QTD_PRODUTO" value="<?php echo $item["NO_CLIENTE"]; ?>" />
-                                        </div>
-                                    </div>
-                                    <div class="control-group">
-                                        <label class="control-label" for="DS_ENDERECO">Endereço:</label>
-                                        <div class="controls">
-                                            <input class="span6" name="DS_ENDERECO" type="text" value="<?php echo $item["DS_ENDERECO"]; ?>" />
-                                        </div>
-                                    </div>
-                                    <div class="controls inline">
-                                    <span id="divTel">
-                                        <input class="span6" name="telefone[nu_telefone][]" type="text" id="NU_TELEFONE" value="<?php echo $item["NU_TELEFONE"]; ?>"/>
-                                        <select name="telefone[cod_tipo_telefone][]">
-                                        <option>Selecionar... </option>>
-                                        <option value="1">Telefone Residencial </option>
-                                        <option value="2">Telefone Comercial </option>
-                                        <option value="3">Telefone Recado </option>
-                                        </select>
-                                    </span>
-                                    <a id="botao" class="botao btn"> <i class="icon-plus"></i></a>
-                                </div>
 
-                                    <div class="control-group">
-                                        <label class="control-label" for="DT_CADASTRO">Data do Cadastro:</label>
-                                        <div class="controls">
-                                            <input class="span6" name="DT_CADASTRO" type="date" id="DT_CADASTRO" value="<?php echo $item["DT_CADASTRO"]; ?>" />
+
+            <div id="container-geral">
+                <div class="container" id="content">
+                    <div class="container-fluid">
+                        <div class="row-fluid">
+                            <div>
+                                <fieldset>
+                                    <legend>Excluir Cliente</legend>
+                                    <form id="cliente" name="excluir" method="post" action="excluirCliente.php">
+                                        <div class="control-group">
+                                            <label class="control-label" for="COD_CLIENTE">Código:</label>
+                                            <div class="controls">
+                                                <input class="span6" name="COD_CLIENTE" readonly="true" id="COD_CLIENTE" value="<?php echo $item["COD_CLIENTE"]; ?>" />
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="control-group">
-                                        <div class="controls">
-                                            <button type="submit" name="excluir" class="btn btn-primary">Excluir</i></button>
-                                            
-                                   </div>
-                           </div>
-                     </form>
-            </fieldset>
-            <?php } ?>
-      </body>
-  
-</html>
+                                        <div class="control-group">
+                                            <label class="control-label" for="NO_CLIENTE">Nome:</label>
+                                            <div class="controls">
+                                                <input class="span6" name="NO_CLIENTE" type="text" id="QTD_PRODUTO" value="<?php echo $item["NO_CLIENTE"]; ?>" />
+                                            </div>
+                                        </div>
+                                        <div class="control-group">
+                                            <label class="control-label" for="DS_ENDERECO">Endereço:</label>
+                                            <div class="controls">
+                                                <input class="span6" name="DS_ENDERECO" type="text" value="<?php echo $item["DS_ENDERECO"]; ?>" />
+                                            </div>
+                                        </div>
+                                        <div class="controls inline">
+                                            <label class="control-label" for="NU_TELEFONE">Telefone:</label>
+                                            <span id="divTel">
+                                                <input class="span6" name="telefone[nu_telefone][]" type="text" id="NU_TELEFONE" value="<?php echo $item["NU_TELEFONE"]; ?>"/>
+                                                <select name="telefone[cod_tipo_telefone][]">
+                                                    <option>Selecionar... </option>>
+                                                    <option value="1">Telefone Residencial </option>
+                                                    <option value="2">Telefone Comercial </option>
+                                                    <option value="3">Telefone Recado </option>
+                                                </select>
+                                            </span>
+                                            <a id="botao" class="botao btn"> <i class="icon-plus"></i></a>
+                                        </div>
+
+                                        <div class="control-group">
+                                            <label class="control-label" for="DT_CADASTRO">Data do Cadastro:</label>
+                                            <div class="controls">
+                                                <input class="span6" name="DT_CADASTRO" type="date" id="DT_CADASTRO" value="<?php echo $item["DT_CADASTRO"]; ?>" />
+                                            </div>
+                                        </div>
+                                        <div class="control-group">
+                                            <div class="controls">
+                                                <button type="submit" name="excluir" class="btn btn-primary">Excluir</i></button>
+
+                                            </div>
+                                        </div>
+                                    </form>
+                                </fieldset>
+                            <?php } ?>
+                            </body>
+
+                            </html>
 
 
 
